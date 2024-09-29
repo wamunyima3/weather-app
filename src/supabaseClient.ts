4edd74b5b6eddd58c157f53c1ea2b790b323a1b6
wamunyima3 <wamunyimamukelabai3@gmail.com>
2024-09-29 07:11:54 +0200

    setup the env file and add the supabase api key and wrote a basic program

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,14 @@
-// /src/supabaseClient.ts
-
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('Supabase Key:', import.meta.env.VITE_SUPABASE_KEY);
+
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL and Key must be provided.');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+interface ProcessEnv {
+    readonly VITE_SUPABASE_URL: string;
+    readonly VITE_SUPABASE_KEY: string;
+    readonly VITE_WEATHER_URL: string;
+    readonly VITE_WEATHER_API: string;
+}
